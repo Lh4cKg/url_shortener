95bef88425e2080e87774b4687ff2b618e24e916
Lasha Gogua <lh4ckg@gmail.com>
2022-11-12 00:08:02 +0400

    added utils

--- url_shortener/apps/utils/__init__.py
+++ url_shortener/apps/utils/__init__.py
@@ -1,5 +1,10 @@
 import uuid
 import random
+from calendar import timegm
+from datetime import datetime
+
+from django.conf import settings
+from django.utils.timezone import is_naive, make_aware, utc
 
 
 def generate_key(url, length=7, shuffle=False):
@@ -20,3 +25,19 @@ async def agenerate_key(url, length=7, shuffle=False):
         return ''.join(random.sample(keys, length))
 
     return key[:length]
+
+
+def make_utc(dt, ):
+    if settings.USE_TZ and is_naive(dt):
+        return make_aware(dt, timezone=utc)
+
+    return dt
+
+
+def datetime_to_epoch(dt):
+    return timegm(dt.utctimetuple())
+
+
+def datetime_from_epoch(ts):
+    return make_utc(datetime.utcfromtimestamp(ts))
+
