11b26ef28f33046574094315a4dba9809c2195ca
Lasha Gogua <lh4ckg@gmail.com>
2022-11-30 01:30:46 +0400

    update uvicorn configuration

--- deploy/us_uvicorn_start.sh
+++ deploy/us_uvicorn_start.sh
@@ -12,5 +12,6 @@ source $ENVBIN/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 export WEB_CONCURRENCY=10
+export WORKER_CLASS="uvicorn.workers.UvicornH11Worker"
 
 exec $ENVBIN/uvicorn --host 0.0.0.0 --port 8000 url_shortener.asgi:application
