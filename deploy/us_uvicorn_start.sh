9a3c5bba45704aa60fa588c4371414a6ebfc4961
Lasha Gogua <lh4ckg@gmail.com>
2022-11-30 01:24:57 +0400

    update uvicorn configuration

--- deploy/us_uvicorn_start.sh
+++ deploy/us_uvicorn_start.sh
@@ -13,4 +13,4 @@ export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 export WEB_CONCURRENCY=10
 
-exec $ENVBIN/uvicorn --host 0.0.0.0 --port 8000 url_shortener.asgi:application
+exec $ENVBIN/uvicorn --host 0.0.0.0 --port 8000 url_shortener.asgi:application --lifespan on
