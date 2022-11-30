48c8ddc5fc2290aeb8315b2da95583304ded763a
Lasha Gogua <lh4ckg@gmail.com>
2022-11-30 17:13:20 +0400

    fixed update url key converter bug

--- url_shortener/apps/shortener/converters.py
+++ url_shortener/apps/shortener/converters.py
@@ -1,7 +1,7 @@
 
 
 class UrlKeyConverter:
-    regex = '[a-zA-Z0-9\-_]{4}'
+    regex = '[a-zA-Z0-9\-_]{4,7}'
 
     def to_python(self, value):
         return value
--- url_shortener/apps/shortener/urls.py
+++ url_shortener/apps/shortener/urls.py
@@ -7,7 +7,7 @@ from .views import UrlRedirectView, ShortenUrlView, ListShortenUrlView
 register_converter(UrlKeyConverter, 'uk')
 
 urlpatterns = [
-    path('<uk:url_key>/', UrlRedirectView.as_view(), name='url_to_redirect'),
     path('api/shorten/', ShortenUrlView.as_view(), name='shorten'),
     path('api/shortens/', ListShortenUrlView.as_view(), name='shorten_list'),
+    path('<uk:url_key>/', UrlRedirectView.as_view(), name='url_to_redirect'),
 ]
